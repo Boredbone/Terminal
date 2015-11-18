221ddf0ef505c848f8c85c53f213db06507b6c57
boredbone <Boredbone@users.noreply.github.com>
2015-11-18 11:45:19 +0900

    ColorConverter

---
+++ XamlTools/Converters/ColorConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Boredbone.XamlTools.Converters
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+
+            var code = value as uint?;
+
+            if (code != null)
+            {
+                var a = (byte)((code.Value & 0xff000000) >> 24);
+                var r = (byte)((code.Value & 0x00ff0000) >> 16);
+                var g = (byte)((code.Value & 0x0000ff00) >> 8);
+                var b = (byte)((code.Value & 0x000000ff) >> 0);
+
+                return new SolidColorBrush(Color.FromArgb(a, r, g, b));
+            }
+
+
+            var array = value as byte[];
+
+            if (array == null || array.Length < 4)
+            {
+                return new SolidColorBrush(Colors.Transparent);
+            }
+            return new SolidColorBrush(Color.FromArgb(array[0], array[1], array[2], array[3]));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
