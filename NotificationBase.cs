b48c85762cf6656106fcb7e74ee8325ce99d851d
boredbone <Boredbone@users.noreply.github.com>
2016-01-26 10:20:42 +0900

    Squashed 'XamlTools/' changes from 6503d0d..a964f58
    a964f58 NotificationBase
    
    git-subtree-dir: XamlTools
    git-subtree-split: a964f5876d4b37fe0477cd4b99610d7d05a1f54c

---
+++ NotificationBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Boredbone.XamlTools
+{
+    public class NotificationBase : DisposableBase, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+            => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
